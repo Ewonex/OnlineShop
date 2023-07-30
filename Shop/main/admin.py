92f11c6390e0a2fe0f106fc3849aa46c738bba51
ewonex <dkrivorot10@gmail.com>
2023-07-30 23:51:06 +0300

    Little fixes.

--- Shop/main/admin.py
+++ Shop/main/admin.py
@@ -6,6 +6,7 @@ class RequestAdmin(admin.ModelAdmin):
     list_filter = ('status',)
 
 admin.site.register(User)
+admin.site.register(Item)
 admin.site.register(Brand)
 admin.site.register(FavoriteItem)
 admin.site.register(Review)
